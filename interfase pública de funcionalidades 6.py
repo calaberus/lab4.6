502b75c39ac1f47b3d467f0ad324fcbb55712c20
calaberus <juan.esteban.mondragon@correounivalle.edu.co>
2024-10-03 10:39:28 -0500

    Add files via upload

---
+++ interfase pública de funcionalidades 6.py
@@ -0,0 +1,19 @@
+class Cafetera:
+
+    def __init__(self, capacidad):
+          self.capacidad = capacidad # Capacidad máxima de la cafetera
+          self._nivel_actual = 0 # Estado interno (nivel de café)
+
+def llenar(self):
+    self._nivel_actual = self.capacidad
+    print("La cafetera ha sido llenada.")
+
+def servir_taza(self):
+    if self._nivel_actual > 0:
+        self._nivel_actual -=1
+    print("se ha servido una taza de café.")
+else:
+    print("No hay café. Llena la cafetera.")
+
+def mostrar_nivel(self):
+     print(f"Nivel actual de café: {self ._ nivel_actual} tazas.")
